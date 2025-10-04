0ed04d34e43ba6139b3724f949d06194e2fd6269
Luiz Felipe <luiz.felipe.reys.2005@gmail.com>
2025-10-04 15:39:55 -0300

    Landing page Concluída

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -10,7 +10,7 @@
     transition: transform 0.3s, box-shadow 0.3s;
   }
   .btn:hover {
-    transform: scale(1.05);
+    transform: translateY(-2px);
     box-shadow: 0 4px 10px rgba(0,0,0,0.2);
   }
 </style>
